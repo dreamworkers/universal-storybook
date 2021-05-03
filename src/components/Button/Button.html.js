34b2a89b4aa38b2f5600070ff568154d95b45bc4
Abhisek Pandey <dreamworkers@gmail.com>
2021-05-03 23:16:12 +0200

    fix: backgroundColor control for HTML button

--- src/components/Button/Button.html.js
+++ src/components/Button/Button.html.js
@@ -1,15 +1,18 @@
 import "./button.css";
 
-export const getClassName = ({ primary, size }) => {
+const getClassName = ({ primary, size }) => {
   const classes = ["button"];
   classes.push(primary ? "button--primary" : "button--secondary");
   if (size) classes.push(`button--${size}`);
   return classes.join(" ");
 };
 
+const getBackgroundColor = (backgroundColor) =>
+  backgroundColor ? `background-color: ${backgroundColor}` : "";
+
 export const ButtonStory = ({ label, backgroundColor, primary, size }) => {
   return `<button type="button" className="${getClassName({
     primary,
     size,
-  })}" style=${backgroundColor && { backgroundColor }} > ${label} </button>`;
+  })}" style="${getBackgroundColor(backgroundColor)}" > ${label} </button>`;
 };
